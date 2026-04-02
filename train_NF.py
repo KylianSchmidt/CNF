dca5795a13fc27c7100ed6f15d6c0511f8b9fa6d
Kylian Schmidt <kylian.schmidt@kit.edu>
2026-04-02 15:42:00 +0200

    Rework arg_parse in NF

--- train_NF.py
+++ train_NF.py
@@ -1,5 +1,5 @@
-import os
 import argparse
+from typing import Literal
 
 import torch
 from torch.utils.data import DataLoader, TensorDataset, random_split
@@ -9,22 +9,36 @@ import numpy as np
 
 from lib.NormFlow import NormalizingFlowModel
 from lib.data_utils import createJetData
+from tabulate import tabulate
 
 
-def prepare_dataloader(args):
+class Args:
+    root_dir: str
+    checkpoint_path: str
+    c: float
+    channel: Literal["signal", "background"]
+    num_jets: Literal[0, 1, 2]
+
+
+def prepare_dataloader(args: Args):
     # Create jet data using the provided utility
-    j2_data, j2_detlabel, _, _ = createJetData(1, False, seed=78,root_dir=args.root_dir)
+    jet_data, jet_detailed_label, _, _ = createJetData(
+        args.num_jets,
+        False,
+        seed=78,
+        root_dir=args.root_dir,
+    )
 
     # Convert data to tensors
-    S_tensor = torch.tensor(j2_data[j2_detlabel == 1], dtype=torch.float32)
-    BG_tensor = torch.tensor(j2_data[j2_detlabel == 0], dtype=torch.float32)
+    S_tensor = torch.tensor(jet_data[jet_detailed_label == 1], dtype=torch.float32)
+    BG_tensor = torch.tensor(jet_data[jet_detailed_label == 0], dtype=torch.float32)
 
     # Equalize dataset size between signal and background
     max_size = np.min([len(S_tensor), len(BG_tensor)])
-    if args.s:
-        dataset = TensorDataset(S_tensor[:max_size], BG_tensor[:max_size])
-    else:
-        dataset = TensorDataset(BG_tensor[:max_size],S_tensor[:max_size])
+    dataset: TensorDataset = {
+        "signal": TensorDataset(S_tensor[:max_size], BG_tensor[:max_size]),
+        "background": TensorDataset(BG_tensor[:max_size], S_tensor[:max_size]),
+    }[args.channel]
 
     # Split dataset into training and validation sets (80/20 split)
     n_val = int(0.2 * len(dataset))
@@ -41,7 +55,8 @@ def prepare_dataloader(args):
 
     return train_loader, val_loader, X_mean, X_std
 
-def main(args):
+
+def main(args: Args):
     # Setup root directory and add necessary paths
     root_dir = args.root_dir
     print("HEP Challenge Root directory is", root_dir)
@@ -53,10 +68,15 @@ def main(args):
     early_stop_callback = EarlyStopping(
         monitor="val_loss", min_delta=0.00, patience=100, mode="min"
     )
-    if args.s:
-        file_name = f"s_advNF_c_{args.c}"
-    else:
-        file_name = f"/bg_advNF_c_{args.c}"
+    file_name: str = {
+        "signal": f"NF_njets_{args.num_jets}_signal_c_{args.c}",
+        "background": f"NF_njets_{args.num_jets}_background_c_{args.c}",
+    }[args.channel]
+
+    input_dim = {
+        1: 20,  # Case: 1 jet
+        2: 27,  # Case: 2 jets
+    }[args.num_jets]
 
     checkpoint_callback = ModelCheckpoint(
         monitor="val_loss",
@@ -64,10 +84,9 @@ def main(args):
         save_top_k=3,
         filename=file_name
     )
-
     bg_model = NormalizingFlowModel(
         c=args.c,
-        input_dim=20,
+        input_dim=input_dim,
         clamp_val=-50,
         n_layers=30,
         X_mean=X_mean,
@@ -91,7 +110,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--root-dir",
         type=str,
-        default="/Users/ibrahim/HEP-Challenge/",
+        default="../HEP-Challenge/",
         help="Root directory containing input_data, ingestion_program, and scoring_program folders"
     )
     parser.add_argument(
@@ -105,11 +124,19 @@ if __name__ == "__main__":
         type=float,
         default=1,
     )
-
     parser.add_argument(
-        "-s",
-        type=bool,
-        default=True,
+        "--channel",
+        type=str,
+        choices=["signal", "background"],
+        default="signal",
+    )
+    parser.add_argument(
+        "--num_jets",
+        type=int,
+        default=1,
     )
     args = parser.parse_args()
+
+    args_dict = vars(args)
+    print(tabulate(args_dict.items(), headers=["Argument", "Value"], tablefmt="simple"))
     main(args)
